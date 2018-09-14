0108a628555274200d76767d33ddf4bef91ea3df
pikashii <mlupoli1@binghamton.edu>
2018-09-14 18:32:14 -0400

    v1.0.1

--- bot.js
+++ bot.js
@@ -4,13 +4,13 @@ const token = 'NDkwMjUwMjYyMzIyMzQ4MDMz.Dn2nDw.jYwEaO0xsUZGbzb048uNyL4AopU'; // 
  
 client.login(token); // login the bot with your token.
  
-client.on('message', message => { // This is where we will handle all message events. Aka, the commands that will trigger the bot.
+client.on('message', message => { //message commands!
  
-    if(message.content.toLowerCase() === 'hanayo')
+    if(message.content.toLowerCase() === 'hanayo') //the standalone message 'hanayo'
         message.channel.send("i love you " + message.author + "!");
-    else if(message.content.toLowerCase().includes('hanayo'))
+    else if(message.content.toLowerCase().includes('hanayo')) //all messages containing 'hanayo'
 	message.channel.send("i love you " + message.author + "!");
-    // etc.... you get the idea.
+    //copy and paste more as needed
  
  
 });#Hanayo
