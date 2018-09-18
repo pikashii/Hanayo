aab7dbd95bc8a9f9b07cc2637f137caa0842c1bb
pikashii <mlupoli1@binghamton.edu>
2018-09-18 11:13:07 -0400

    1.0.8

--- bot.js
+++ bot.js
@@ -1,16 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = ''; // token omitted.
+const token = 'NDkwMjUwMjYyMzIyMzQ4MDMz.Dn2nDw.jYwEaO0xsUZGbzb048uNyL4AopU'; // Token goes here.
+
  
-client.login(token);
+client.login(token); // login the bot with your token.
  
-client.on('message', message => { //message commands!
+client.on('message', message => { // This is where we will handle all message events. Aka, the commands that will trigger the bot.
  
-    if(message.content.toLowerCase() === 'hanayo') //the standalone message 'hanayo'
+    if(message.content.toLowerCase() === 'hanayo') 
         message.channel.send("i love you " + message.author + "!");
-    else if(message.content.toLowerCase().includes('hanayo')) //all messages containing 'hanayo'
+    else if(message.content.toLowerCase().includes('hanayo')) 
 	message.channel.send("i love you " + message.author + "!");
-    //copy and paste more as needed
- 
- 
-});#Hanayo
+
+    if(message.content.toLowerCase() === 'rin') 
+        message.channel.send("i love rin-chan " + "!");
+    else if(message.content.toLowerCase().includes('rin')) 
+	message.channel.send("i love rin-chan " + "!");
+
+    if(message.content.toLowerCase() === 'maki') 
+        message.channel.send("i love maki-chan " + "!");
+    else if(message.content.toLowerCase().includes('maki')) 
+	message.channel.send("i love maki-chan " + "!");
+
+    if(message.content.toLowerCase() === 'nozomi') 
+        message.channel.send("thanos idol");
+    else if(message.content.toLowerCase().includes('nozomi')) 
+	message.channel.send("thanos idol");
+
+// etc.... you get the idea.
+
+});
