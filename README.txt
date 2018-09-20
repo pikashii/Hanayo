146ed91be1fa406499189a4ed152197a1f1d7d2f
pikashii <mlupoli1@binghamton.edu>
2018-09-20 19:10:57 -0400

    1.1.0

--- README.txt
+++ README.txt
@@ -3,15 +3,18 @@ This bot is experimental and there's no gurantee it will always work as it shoul
 Hanayo responds to all mentions of the keyword 'hanayo'.
 She will respond with "i love you!" and @ the user.
 
-As of v1.0.10, more keywords have been added.
-Hanayo will additionally respond to the following keywords:
+As of v1.0.10, more phrases have been added.
+Hanayo will additionally respond to the following phrases:
 - rin
 - maki
 - umi
 - nozomi
 - rice
+- scout
+- thanos car
+- know i had to do it to em
 
-PLUS two secret words! 
+If you'd like to have Hanayo generate an invite link, send the following message:
+- x3 invite
 
-Add Hanayo to your server:
-https://discordapp.com/oauth2/authorize?client_id=490250262322348033&scope=bot&permissions=0
+If there are any issues with Hanayo spamming messages or responding when she shouldn't, please report them to me in the issues tab and I'll fix it as soon as possible.
--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = ''; // Token goes here.
+const { Client, Attachment } = require('discord.js');
 
- 
-client.login(token); // login the bot with your token.
- 
-client.on('message', message => { 
- 
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('ready!');
+});
+
+client.on('message', message => {
+
     if(message.content.toLowerCase() === 'hanayo') 
         message.channel.send("i love you " + message.author + "!");
     else if(message.content.toLowerCase().includes('hanayo')) 
@@ -32,17 +33,26 @@ client.on('message', message => {
     else if(message.content.toLowerCase().includes('rice')) 
 	message.channel.send("pwease " + message.author + "! " + "pwease share some of your wice!");
 
-if(message.content.toLowerCase() === 'scout') 
+    if(message.content.toLowerCase() === 'scout') 
         message.channel.send(message.author + ", i hope you get a ur!");
     else if(message.content.toLowerCase().includes('scout')) 
 	message.channel.send(message.author + ", i hope you get a ur!!");
 
-if(message.content.toLowerCase() === 'thanos car') 
+    if(message.content.toLowerCase() === 'thanos car') 
         message.channel.send("does thanos car is fuck?");
 
-if(message.content.toLowerCase() === 'umi') 
+    if(message.content.toLowerCase() === 'umi') 
         message.channel.send("ocean man, take me by the hand");
     else if(message.content.toLowerCase().includes('umi')) 
 	message.channel.send("ocean man, take me by the hand");
 
+    if(message.content.toLowerCase() === 'x3 invite') 
+        message.channel.send("https://discordapp.com/oauth2/authorize?client_id=490250262322348033&scope=bot&permissions=0");
+
+    if(message.content.toLowerCase().includes('had to do it to em')) {
+        const attachment = new Attachment('https://i.imgur.com/aIq162d.jpg');
+        message.channel.send(attachment);
+    }
 });
+
+client.login('');
